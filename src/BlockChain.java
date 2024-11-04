78257f28bb4a43b72974ea209d10258352f7e01b
MariamAziz0 <ma27523@gmail.com>
2024-11-04 11:38:08 +0200

    Some methods in blockchain are implemented

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -23,6 +23,7 @@
 
 public class BlockChain {
     public static final int CUT_OFF_AGE = 10;
+    private TransactionPool transactionPool;
 
     /**
      * create an empty blockchain with just a genesis block. Assume {@code genesisBlock} is a valid
@@ -30,6 +31,8 @@ public class BlockChain {
      */
     public BlockChain(Block genesisBlock) {
         // IMPLEMENT THIS
+        transactionPool = new TransactionPool();
+        // Not Finished
     }
 
     /** Get the maximum height block */
@@ -47,28 +50,33 @@ public class BlockChain {
     /** Get the transaction pool to mine a new block */
     public TransactionPool getTransactionPool() {
         // IMPLEMENT THIS
-        return null;
+        return transactionPool;
     }
 
     /**
      * Add {@code block} to the blockchain if it is valid. For validity, all transactions should be
      * valid and block should be at {@code height > (maxHeight - CUT_OFF_AGE)}, where maxHeight is 
      * the current height of the blockchain.
-	 * <p>
-	 * Assume the Genesis block is at height 1.
+	   * <p>
+	   * Assume the Genesis block is at height 1.
      * For example, you can try creating a new block over the genesis block (i.e. create a block at 
-	 * height 2) if the current blockchain height is less than or equal to CUT_OFF_AGE + 1. As soon as
-	 * the current blockchain height exceeds CUT_OFF_AGE + 1, you cannot create a new block at height 2.
+	   * height 2) if the current blockchain height is less than or equal to CUT_OFF_AGE + 1. As soon as
+	   * the current blockchain height exceeds CUT_OFF_AGE + 1, you cannot create a new block at height 2.
      * 
      * @return true if block is successfully added
      */
     public boolean addBlock(Block block) {
         // IMPLEMENT THIS
-        return false;
+        if (block.getPrevBlockHash() == null) {
+            return false;
+        }
+        // Not Finished
+        return true;
     }
 
     /** Add a transaction to the transaction pool */
     public void addTransaction(Transaction tx) {
         // IMPLEMENT THIS
+        transactionPool.addTransaction(tx);
     }
 }
